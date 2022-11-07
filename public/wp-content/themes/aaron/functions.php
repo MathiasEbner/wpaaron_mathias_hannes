6e1a59da035990957e87904e21431c73567bce6c
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2022-11-07 14:29:42 +0100

    After new WP-installation

--- public/wp-content/themes/aaron/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    function wpdocs_theme_name_scripts(){
-        wp_enqueue_style( 'style', get_stylesheet_uri() );
-    }
-    add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
-?>
