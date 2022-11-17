ba8c0a3c403ecccc9e2721614a9d72de6e509e45
Mathias Ebner <fhs47758@fh-salzburg.ac.at>
2022-11-17 21:02:05 +0100

    deploy 4

--- deploy.php
+++ deploy.php
@@ -32,12 +32,6 @@ host('vm-ultramarine.multimediatechnology.at')
 // Composer
 set('composer_action', false);
 
-task('deploy', [
-    'deploy:unlock',
-    'deploy:prepare',
-    'deploy:publish'
-]);
-
 // Tasks
 desc('Deploy your project');
 task('deploy', [
